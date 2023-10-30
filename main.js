1df764dfcc75fe5975691070242541ca78cdb4e0
FrostdFlake <hellothisisfrost@gmail.com>
2023-10-29 21:57:43 -0400

    gifs linked up

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const sectEight = document.createElement("section");
 mainDoc.prepend(firstTitle, daDiv, sectEight);
 const crowdSurf = document.querySelector("iframe");
 const msMonet = document.querySelectorAll("img")
-daDiv.append(crowdSurf, ...msMonet);
+daDiv.append(msMonet[0], crowdSurf, msMonet[1]);
 const textNdWerds = document.querySelectorAll("ul");
 sectEight.append(...textNdWerds);
 sectEight.style.display = "flex";
@@ -22,6 +22,16 @@ titleElements.forEach(function(titleElement) {
     element.classList.toggle("light-mode");
   });
 });
+
+msMonet[0].addEventListener("click", function() {
+      const youtubeURL = "https://youtu.be/KdJ-Qwu3y4Y?si=w7wyVVZ5ryk8ENoe";
+      window.open(youtubeURL, "_blank");
+    });
+
+msMonet[1].addEventListener("click", function() {
+      const utubeURL = "https://youtu.be/1RFLY62LrH0?si=ji6n1f6aVjl-oSI9";
+      window.open(utubeURL, "_blank");
+    });
 
 document.getElementById("form").addEventListener("submit", function(event) {
   event.preventDefault();
